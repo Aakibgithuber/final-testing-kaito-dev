db992061ff6d409fdbd53726a6885b3f7ffa6256
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-27 23:06:33 +0530

    Update deploy-stack.ts

--- lib/deploy-stack.ts
+++ lib/deploy-stack.ts
@@ -2,40 +2,52 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { S3BucketResource } from './resources/s3';
 import { ElasticBeanstalkResource } from './resources/ElasticBeanstalk';
-import { IAMResource } from './resources/IAM';
-import { LambdaResource } from './resources/Lambda';
-import { CloudFrontResource } from './resources/CloudFront';
+import { IAMInstanceProfile } from './resources/IAM';
+import { LambdaFunction } from './resources/Lambda';
+import { CloudFrontDistribution } from './resources/CloudFront';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); 
+dotenv.config();
 
-
-// 🔹 Main Stack Definition
 export class KaitoApplicationStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        // Initialize Resources
-        const s3Bucket = new S3BucketResource(this);
-        const beanstalk = new ElasticBeanstalkResource(this);
-        const iam = new IAMResource(this);
-        const lambda = new LambdaResource(this);
-        const cloudfront = new CloudFrontResource(this);
+        // S3 Bucket
+        const s3Bucket = new S3BucketResource(this, 'MyBucket1', {
+            bucketName: `${process.env.APP_NAME}-${process.env.NODE_ENV}-client1`.toLowerCase(),
+            versioned: true,
+            publicReadAccess: false
+        });
 
-        // S3 Buckets
-        const myBucket = s3Bucket.create('MyBucket1', `${process.env.APP_NAME}-${process.env.NODE_ENV}-client1`.toLowerCase());
+        // IAM Role & Instance Profile
+        const instanceProfile1 = new IAMInstanceProfile(this, 'MyBeanstalkInstanceRole1', {
+            roleName: 'MyBeanstalkRole1'
+        });
 
-        // Create Elastic Beanstalk App & Env
-        const ebApp1 = beanstalk.createApplication('MyElasticBeanstalkApp1', "kaito-eb-app-1");
-        const instanceProfile1 = iam.createInstanceRole('MyBeanstalkInstanceRole1');
-        const ebEnv1 = beanstalk.createEnvironment('MyElasticBeanstalkEnv1', ebApp1.applicationName!, instanceProfile1.ref);
+        // Elastic Beanstalk
+        const ebApp1 = new ElasticBeanstalkResource(this, 'MyElasticBeanstalkApp1', {
+            appName: "kaito-eb-app-1",
+            instanceProfileRef: instanceProfile1.ref
+        });
 
-        // Create Lambda Function
-        const schemaCreatorLambda1 = lambda.SchemaCreator('SchemaCreatorLambda1');
+        // Lambda Function
+        const schemaCreatorLambda1 = new LambdaFunction(this, 'SchemaCreatorLambda1', {
+            functionName: 'SchemaCreatorLambda',
+            entryPath: 'lib/lambda/schema_creator.ts',
+            environment: {
+                DB_HOST: process.env.DB_HOST!,
+                DB_DATABASE: process.env.DB_DATABASE!,
+                DB_USER: process.env.DB_USER!,
+                DB_PASSWORD: process.env.DB_PASSWORD!,
+                DB_PORT: process.env.DB_PORT || '5432',
+                DB_SCHEMA: process.env.DB_SCHEMA!
+            }
+        });
 
         // Outputs
-        new cdk.CfnOutput(this, 'BucketName', { value: myBucket.bucketName });
-        new cdk.CfnOutput(this, 'ElasticBeanstalkEnv1', { value: ebEnv1.ref });
+        new cdk.CfnOutput(this, 'BucketName', { value: s3Bucket.bucketName });
+        new cdk.CfnOutput(this, 'ElasticBeanstalkEnv1', { value: ebApp1.applicationName! });
         new cdk.CfnOutput(this, 'Lambda1', { value: schemaCreatorLambda1.functionName });
     }
 }
