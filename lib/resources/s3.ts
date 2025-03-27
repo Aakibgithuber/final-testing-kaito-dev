559760dad51b9b09f973c06ff16917676dad2f95
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-03-27 23:02:26 +0530

    Update s3.ts

--- lib/resources/s3.ts
+++ lib/resources/s3.ts
@@ -1,19 +1,22 @@
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 
-export class S3BucketResource {
-    scope: Construct;
-    
-    constructor(scope: Construct) {
-        this.scope = scope;
-    }
+interface S3Props {
+    bucketName: string;
+    versioned?: boolean;
+    publicReadAccess?: boolean;
+}
 
-    create(id: string, bucketName: string) {
-        return new s3.Bucket(this.scope, id, {
-            bucketName, 
-            versioned: true,
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
+export class S3BucketResource extends s3.Bucket {
+    constructor(scope: Construct, id: string, props: S3Props) {
+        super(scope, id, {
+            bucketName: props.bucketName,
+            versioned: props.versioned ?? true,
+            publicReadAccess: props.publicReadAccess ?? false,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+            autoDeleteObjects: false
         });
     }
 }
