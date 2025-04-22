07ac074e3d2272511b929b3be26ab36f0d7de584
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-04-22 15:50:50 +0530

    Update cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { KaitoApplicationStack } from '../lib/deploy-stack';
-import { Kaitos3stack } from '../lib/s3-stack';
+import { BackendApplicationStack } from '../lib/s3-stack';
 import * as projectConfig from '../config/projectConfig.json';
 
 const app = new cdk.App();
