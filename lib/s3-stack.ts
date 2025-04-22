dbd7c3ace970571087fa19b2f1f344201ed10305
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-04-22 15:23:45 +0530

    Update s3-stack.ts

--- lib/s3-stack.ts
+++ lib/s3-stack.ts
@@ -1,21 +1,26 @@
+// s3-stack.ts
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { S3BucketResource } from './resources/s3';
 
-// 🔹 Main Stack Definition
-export class Kaitos3stack extends cdk.Stack {
-    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+interface BackendStackProps extends cdk.StackProps {
+  tenantName: string;
+}
 
-        // S3 Buckets
-        const s3Bucket = new S3BucketResource(this, 'MyBucket1', {
-            bucketName: `${process.env.APP_NAME}-${process.env.NODE_ENV}-client1`.toLowerCase(),
-            versioned: true,
-            publicReadAccess: false
-        });
+export class BackendApplicationStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props: BackendStackProps) {
+    super(scope, id, props);
 
+    const { tenantName } = props;
 
-        // Outputs
-        new cdk.CfnOutput(this, 'BucketName', { value: s3Bucket.bucketName });
-    }
+    const bucketName = `kaito-prod-${tenantName}`.toLowerCase();
+
+    const s3Bucket = new S3BucketResource(this, `s3-${tenantName}`, {
+      bucketName: bucketName,
+      versioned: true,
+      publicReadAccess: false
+    });
+
+    new cdk.CfnOutput(this, 'BucketName', { value: s3Bucket.bucketName });
+  }
 }
