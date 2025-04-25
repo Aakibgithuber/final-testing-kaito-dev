acd978ab536adf9789f90f569ad54e479cb98280
Aakib khan <101191261+Aakibgithuber@users.noreply.github.com>
2025-04-25 08:29:49 +0530

    Update cdk.ts

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -7,17 +7,17 @@ import * as projectConfig from '../config/projectConfig.json';
 const app = new cdk.App();
 const tenantName = process.env.TENANT_NAME || 'default-tenant';
 
-// new KaitoApplicationStack(app, 'KaitoApplicationStack', {
-//   stackName: `${projectConfig.projectPrefix}-pipeline-stack`,
-//   env: {
-//     region: projectConfig.environments.DEV.region,
-//   },
-// });
-
-new BackendApplicationStack(app, `${tenantName}-BackendApplicationStack`, {
-  tenantName: tenantName,
-  stackName: `${projectConfig.projectPrefix}-${tenantName}-s3-creation-stack`,
+new KaitoApplicationStack(app, 'KaitoApplicationStack', {
+  stackName: `${projectConfig.projectPrefix}-pipeline-stack`,
   env: {
     region: projectConfig.environments.DEV.region,
   },
 });
+
+// new BackendApplicationStack(app, `${tenantName}-BackendApplicationStack`, {
+//   tenantName: tenantName,
+//   stackName: `${projectConfig.projectPrefix}-${tenantName}-s3-creation-stack`,
+//   env: {
+//     region: projectConfig.environments.DEV.region,
+//   },
+// });
